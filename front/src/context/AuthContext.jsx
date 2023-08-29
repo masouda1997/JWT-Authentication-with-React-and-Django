f88ea48bde21801254cd9746e951490c0727489c
Automation MotaharZamir <Aut.mz@entekhab.local>
2023-08-29 16:48:02 +0330

    note with error

--- front/src/context/AuthContext.jsx
+++ front/src/context/AuthContext.jsx
@@ -79,6 +79,7 @@ export const AuthProvider = ({ children }) => {
 
     let contextData = {
         user: user,
+        authToken: authToken,
         loginUser: loginUser,
         logoutUser: logoutUser,
     };
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Header } from '../components/Header'
+import React from "react";
+import { Header } from "../components/Header";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 export const Home = () => {
-  return (
-    <>
-        <Header/>
-        <div>Home page  </div>
-    </>
-  )
-}
+    const [notes, setNotes] = useState([]);
+    let { authToken } = useContext(AuthContext);
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    const getNotes = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/notes/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + String(authToken.access),
+            },
+        });
+        const data = await response.json();
+        setNotes(data);
+    };
+
+    return (
+        <>
+            <Header />
+            <div>Home page </div>
+            <ul>
+                {notes.map((note) => (
+                    <li key={note.id}>{note.body}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
